a84ccb48ce42ddb520c5860196fcaef629b02ff3
Christophe Prevost <chry270317@gmail.com>
2025-10-18 18:59:42 +0200

    plus

--- java.js
+++ java.js
@@ -2,3 +2,7 @@ const carte = document.getElementById("carteflip");
 carte.addEventListener("click", () => {
   carte.classList.toggle("flipped");
 });
+const carte2 = document.getElementById("carteflip2");
+carte2.addEventListener("click", () => {
+  carte2.classList.toggle("flipped");
+});
