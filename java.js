f85cb6adfe5fa567a7caae4c02b0a62c4eb627b5
Christophe Prevost <chry270317@gmail.com>
2025-10-28 00:36:24 +0100

    rajout

--- java.js
+++ java.js
@@ -1,12 +1,25 @@
 const carte = document.getElementById("carteflip");
-carte.addEventListener("click", () => {
-  carte.classList.toggle("flipped");
-});
 const carte2 = document.getElementById("carteflip2");
-carte2.addEventListener("click", () => {
-  carte2.classList.toggle("flipped");
-});
 const carte3 = document.getElementById("carteretour");
-carte3.addEventListener("click", () => {
-  carte3.classList.toggle("flipped");
-});
+const carte4 = document.getElementById("retourne");
+
+if (carte) {
+  carte.addEventListener("click", () => {
+    carte.classList.toggle("flipped");
+  });
+}
+if (carte2) {
+  carte2.addEventListener("click", () => {
+    carte2.classList.toggle("flipped");
+  });
+}
+if (carte3) {
+  carte3.addEventListener("click", () => {
+    carte3.classList.toggle("flipped");
+  });
+}
+if (carte4) {
+  carte4.addEventListener("click", () => {
+    carte4.classList.toggle("flipped");
+  });
+}
